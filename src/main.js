cbb5d672bc4066504813aa03a6ebe4d7c13aced4
monster <monster@hamsterbase.com>
2022-06-16 22:36:31 +0800

    feat: update

---
+++ src/main.js
@@ -0,0 +1,14 @@
+const { fork } = require('child_process');
+const { join } = require('path');
+
+const server = join(__dirname, 'server/main.js');
+const currentDir = join(require('os').homedir(), 'hamsterbase');
+
+fork(server, process.argv.slice(2), {
+  env: {
+    DATABASE_DIR: join(currentDir, 'database'),
+    DOC_DIR: join(__dirname, 'docs'),
+    WEB_RESOURCES: join(__dirname, 'web'),
+    SIMPLE: join(__dirname, 'simple'),
+  },
+});
