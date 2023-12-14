7c2306ef579d6938fdd38b1d8dd4f59ab475f0d2
monster <monster@hamsterbase.com>
2023-12-14 20:46:23 +0800

    chore: upgrate release

--- release/hamsterbase-selfhosted-0.9.2/server/main.js
+++
@@ -1,2 +0,0 @@
-"use strict";var main=require("./main-7fd124a9.js");require("stream"),require("http"),require("url"),require("punycode"),require("https"),require("zlib"),require("fs/promises"),require("path"),require("os"),require("fs"),require("crypto"),require("events"),require("net"),require("tls"),module.exports=main.startHamsterBase;
-//# sourceMappingURL=main.js.map
